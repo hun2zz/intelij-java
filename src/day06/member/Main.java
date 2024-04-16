33437ab43edda265ccb00a4fcaafdb0e28d5dbab
hun2zz <tkdgnsdldkdlel@naver.com>
2024-04-16 11:59:52 +0900

    :white_check_mark: #회원관리 시스템 -회원 정보 생성, 전체 조회 처리

--- src/day06/member/Main.java
+++ src/day06/member/Main.java
@@ -8,22 +8,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SimpleInput si = new SimpleInput();
-        //회원 정보를 입력 받아서 회원 배열에 저장
-        Member[] members = {};
 
-        String email = si.input("- 이메일: ");
+        MemberController mc = new MemberController();
 
-        String name = si.input("- 이름: ");
-
-        String password = si.input("- 패스워드:");
-        String gender = si.input("- 성별:");
-        int age = Integer.parseInt(si.input("-나이:"));
-
-        // 입력데이터를 기반으로 한 명의 회원 객체를 생성
-        Member newMember = new Member(email,name,password,gender,age);
-        //생성된 객체를 회원 배열에 저장
-
+        mc.run();
 
     }
 }
--- src/day06/member/MemberRepository.java
+++ src/day06/member/MemberRepository.java
@@ -13,6 +13,11 @@ public class MemberRepository {
         members[2] = new Member("123@def.com", "1111", "명수", "남성", 10);
     }
 
+
+    /**
+     * 생성된 회원정보를 회원 배열에 끝에 추가하는 기능
+     * @param newMember - 사용자의 입력으로 전달된 회원 정보 객체
+     */
     void addNewMember(Member newMember) {
         //배열에 데이터를 추가하는 로직
         Member[] temp = new Member[members.length + 1];
@@ -22,6 +27,21 @@ public class MemberRepository {
         }
         temp[temp.length -1] = newMember;
         members = temp;
+    }
 
+
+    /**
+     * 이메일 중복을 확인하는 기능
+     * @param targetEmail - 검사할 사용자의 입력 이메일 값
+     * @return - 이메일이 이미 존재한다면 true, 존재하지 않은 사용가능한 이메일임.
+     */
+
+    boolean isDuplicateEmail(String targetEmail) {
+        for (Member member : members) {
+            if (targetEmail.equals(member.email)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
