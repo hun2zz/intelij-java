c8f40a62a58cceba787a13fe2e04ecc4f1e986b1
hun2zz <tkdgnsdldkdlel@naver.com>
2024-04-16 09:56:09 +0900

    :white_check_mark: #회원관리 시스템 -회원정보 객체 설계 수강

---
+++ src/day06/member/Test.java
@@ -0,0 +1,24 @@
+package day06.member;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Member[] arr = new Member[5];
+
+
+
+
+        Member m1 = new Member("abc@def.com", "1234", "김철수", "남성", 30);
+        Member m2 = new Member("xyz@ghi.com", "1521", "김영희", "여성", 32);
+
+        arr[0] = m1;
+        arr[1] = m2;
+        arr[2] = new Member("ad","zz","d","d",30);
+
+        System.out.println("m1 = " + m1.toString());
+        System.out.println("m2 = " + m2.toString());
+
+
+        Member[] members = {m1, m2};
+    }
+}
