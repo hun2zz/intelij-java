d58d963d28ca71ddee809e11bcd9f5dc35449cb6
hun2zz <tkdgnsdldkdlel@naver.com>
2024-04-18 22:27:29 +0900

    :white_check_mark: day 08

---
+++ src/day08/protec/pac2/C.java
@@ -0,0 +1,11 @@
+package day08.protec.pac2;
+
+import day08.protec.pac1.A;
+
+public class C {
+
+    void test() {
+        A a = new A();
+
+    }
+}
---
+++ src/day08/protec/pac2/D.java
@@ -0,0 +1,20 @@
+package day08.protec.pac2;
+
+import day08.protec.pac1.A;
+
+public class D extends A {
+    D(){
+        super(5);
+
+        f1 = 100;
+//        f2 = 200;
+
+        m1();
+//        m2
+    }
+
+    void test(){
+        A a = new A();
+//        a.f1= 10; a.f2 = 30;
+    }
+}
